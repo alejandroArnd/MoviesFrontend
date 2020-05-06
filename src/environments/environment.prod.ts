9252d302145e7aa367d1a6cf07e7e9bf12c62c8c
Alejandro Aranda <alexarnd99@gmail.com>
2020-05-06 15:04:38 +0200

    feat(env): Created enviroments for url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  REST_API_SERVER :'http://vps722937.ovh.net:8000/',
+  RECENTMOVIES:'recentmovies',
+  GENRES:'genres',
+  SEARCHMOVIES:'search',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  REST_API_SERVER :'http://vps722937.ovh.net:8000/',
+  RECENTMOVIES:'recentmovies',
+  GENRES:'genres',
+  SEARCHMOVIES:'search',
 };
 
 /*
