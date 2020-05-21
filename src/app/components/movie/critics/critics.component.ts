391f97161c55d07a8e39ca34af8a8674b6a3a661
Alejandro Aranda <alexarnd99@gmail.com>
2020-05-21 21:33:02 +0200

    feat(critics) Created component critics

---
+++ src/app/components/movie/critics/critics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-critics',
+  templateUrl: './critics.component.html',
+  styleUrls: ['./critics.component.scss']
+})
+export class CriticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
