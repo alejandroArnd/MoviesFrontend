465c65e9309893ca5cd55a016877695c7eff8061
Alejandro Aranda <alexarnd99@gmail.com>
2020-06-09 18:50:17 +0200

    feat(admin): Delete endpoints of admin genres

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -17,19 +17,6 @@ export class AdminService {
   public sendDeleteUser(idUser){
     return this.httpclient.delete(environment.REST_API_SERVER+environment.DELETEUSER+'/'+idUser);
   }
-
-  public sendGetAllGenres(currentPage){
-    return this.httpclient.post(environment.REST_API_SERVER+environment.ALLGENRES,{'currentPage':currentPage});
-  }
-  public sendCreateGenre(genre){
-    return this.httpclient.post(environment.REST_API_SERVER+environment.CREATEGENRE,genre);
-  }
-  public sendDeleteGenre(idGenre){
-    return this.httpclient.delete(environment.REST_API_SERVER+environment.DELETEGENRE+'/'+idGenre);
-  }
-  public sendUpdateGenre(genre){
-    return this.httpclient.post(environment.REST_API_SERVER+environment.UPDATEGENRE,genre);
-  }
   public sendGetAllCritics(currentPage){
     return this.httpclient.post(environment.REST_API_SERVER+environment.ALLCRITICS,{'currentPage':currentPage});
   }
