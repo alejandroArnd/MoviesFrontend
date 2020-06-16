0a91bad56aee1b79ef2a2a89b4d5e2d9f132a5c9
Alejandro Aranda <alexarnd99@gmail.com>
2020-06-16 18:18:36 +0200

    feat(searchresult): Add ngOndestroy for route susbcribe

--- src/app/components/searchresult/searchresult.component.ts
+++ src/app/components/searchresult/searchresult.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit} from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { SearchService } from 'src/app/services/search.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-searchresult',
   templateUrl: './searchresult.component.html',
   styleUrls: ['./searchresult.component.scss']
 })
-export class SearchresultComponent implements OnInit {
+export class SearchresultComponent implements OnInit, OnDestroy {
   currentPage;
   genresSelected;
   title;
   movies=[]
   totalItems
+  route$:Subscription;
   constructor(private searchService:SearchService, private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe( params => {
+    this.route$=this.route.queryParamMap.subscribe( params => {
       this.currentPage=params.get('page');
       this.genresSelected=params.getAll('genre');
       this.title=params.get('title').replace("_"," ");
@@ -42,6 +44,10 @@ export class SearchresultComponent implements OnInit {
       queryParams.genre=this.genresSelected;
     }
     this.router.navigate(['/search'], { queryParams: queryParams });
+}
+
+ngOnDestroy(){
+  this.route$.unsubscribe();
 }
 
 }
